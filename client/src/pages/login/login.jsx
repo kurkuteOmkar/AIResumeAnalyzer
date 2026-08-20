507a64335a16758d13cc0e220ea7b6d9f1b35fc9
Omkar <kurkuteomkar9@gmail.com>
2026-08-20 13:18:22 +0530

    login api change

--- client/src/pages/login/login.jsx
+++ client/src/pages/login/login.jsx
@@ -30,7 +30,7 @@ return
     return
    }
 try{
-   const response=await axios.post(`${import.meta.env.VITE_API_UR}/login`,loginInfo)
+   const response=await axios.post(`${import.meta.env.VITE_API_URL}/login`,loginInfo)
    console.log(response.data)
    if(!response.data.token){
     alert("Please login again")
